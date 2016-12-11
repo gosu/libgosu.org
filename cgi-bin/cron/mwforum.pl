54873cee7c37fcfe404db3bce20ea60e0a3190b7
Julian Raschke <julian@raschke.de>
2016-12-11 21:02:14 +0100

    Fix cronjobs (RSS feeds etc.)

--- cgi-bin/cron/mwforum.pl
+++ cgi-bin/cron/mwforum.pl
@@ -6,14 +6,14 @@ use strict;
 use warnings;
 use Fcntl qw(:flock);
 
-flock(DATA, LOCK_EX|LOCK_NB) or die "Must not run in parallel";
+flock(DATA, LOCK_EX|LOCK_NB) or die "Only one instance of this script can run at the same time.";
 
-# Go to mwForum CGI bin.
 chdir "../mwf";
-
-print `perl cron_jobs.pl`;
-print `perl cron_subscriptions.pl`;
-print `perl cron_rss.pl`;
+# Run all scripts with `env -i` to prevent CGI variables from leaking into sub-processes.
+# (The cron_*.pl scripts cannot run in a CGI environment.)
+print `env -i perl cron_jobs.pl 2>&1`;
+print `env -i perl cron_subscriptions.pl 2>&1`;
+print `env -i perl cron_rss.pl 2>&1`;
 
 __DATA__
 flock
